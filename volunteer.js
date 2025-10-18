acd03e1d223176ac0cf892acc9a208a7bd20073d
alexj-7827 <alexjmnz2005@gmail.com>
2025-10-17 22:49:46 -0500

    Assignment 3: Added additional functions to volunteer.js.

--- volunteer.js
+++ volunteer.js
@@ -5,10 +5,11 @@
 /**
  * Constructor: Create volunteering event object
  * An event object will include its name, urgency, location, date, description, and required skills.
+ * For Assignment 3, the attendance list will be a boolean. In the final project, the list will be an array.
  */
 function VolEvent(name, urgency, location, date, desc, skills) {
     this.name = name;
-    this.urgency = new Int(urgency);
+    this.urgency = new Int(urgency); // 0 = Low, 1 = Medium, 2 = High
     this.location = location;
     this.date = new Date(date);
     this.desc = desc;
@@ -34,13 +35,44 @@ function Volunteer(nameF, nameL, skills, preferences, dates) {
  * Function: Fetch volunteer history
  * This is used for volunteer_history. It gathers past events to include in displaying volunteering history.
  */
+function getHistory(volevents){
+    this.volevents = new Array(volevents);
+    if (this.volevents.length() === 0) return 0;
+    else
+        this.volevents.forEach(function() {
+            
+        });
+}
 
 /**
- * Function: Fetch match to existing volunteer events
+ * Function: Fetch matches to existing volunteer events
  * Look at preexisting future events and see if the volunteer will be a match.
  * This is used for volunteer_matching_form.
+ * For Assignment 3, a hardcoded set of events will be used.
  */
+function getMatches(volprofile){
+    this.profile = new Array(volprofile);
+    if (this.volprofile.length() === 0) return 0;
+    else
+        this.volprofile.forEach(function() {
+            
+        });
+}
 
 /**
  * Below are some hard-coded events for Assignment 3.
  */
+const event1 = new VolEvent('Little League Baseball Game', 1, 'Houston, TX', '2025-10-17',
+    'At 6:00 PM on October 17th, we will host a Little Leagua baseball game for the Houston area.',
+    [1, 4, 5]);
+const event2 = new VolEvent('Fun Run', 2, 'Houston, TX', '2025-10-18',
+    'At 2:00 PM on October 18th, we will host a fun run for the Houston area for elementary school students.',
+    [3, 2, 7]);
+const event3 = new VolEvent('Little League Baseball Game', 1, 'Houston, TX', '2025-10-17',
+    'At 6:00 PM on October 17th, we will host a Little Leagua baseball game for the Houston area.',
+    [1, 4, 5]);
+
+/**
+ * Below are some hard-coded events for Assignment 3.
+ */
+const Mii = new Volunteer('Jason', 'Adams', [1, 4]);
