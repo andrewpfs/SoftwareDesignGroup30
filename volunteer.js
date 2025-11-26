5fcbe4197bee485d455fb62602a9924867a7e547
alexj-7827 <alexjmnz2005@gmail.com>
2025-11-26 17:01:03 -0600

    Assignment 3: Preparing setMatches
    setMatches will be a function that takes auto-matched volunteers, categorizes them by event, and lets admins match or reject them from an event. Upon doing either or pressing the refresh button in the HTML file, the function will run again, clearing the list of events and volunteers and filling them with an updated list.

--- volunteer.js
+++ volunteer.js
@@ -1,6 +1,7 @@
+import React from 'react';
 /*
 Note: This script will be shared between both volunteer_history and volunteer_matching_form.
-As of 11/25/25 at 7:12 PM, this is still a work-in-progress. NodeJS and React have yet to be implemented.
+As of 11/26/25 at 4:56 PM, this is still a work-in-progress. NodeJS and React have yet to be implemented.
 Also, the structure of classes and objects may change based on how they have been implemented in Assignment 4.
 */
 
@@ -11,15 +12,18 @@ An ID will also be assigned to make finding the event easier.
 For Assignment 3, the attendance list will be a boolean. In the final project, the list will be an array of volunteer IDs.
 Also, the final project will have IDs automatically assigned.
 */
-function VolEvent(id, name, urgency, location, date, desc, skills, attendees) {
+function VolEvent(id, name, urgency, location, date, desc, skills, attendees, review, include, exclude) {
     this.id = id;
     this.name = name;
-    this.urgency = new Int(urgency); // 0 = Low, 1 = Medium, 2 = High
+    this.urgency = new Int(urgency); // 0 = Low, 1 = Medium, 2 = High, 3 = Critical
     this.location = location;
     this.date = new Date(date);
     this.desc = desc;
     this.skills = new Array(skills); // Can hold up to 3 skills.
     this.attendees = new Boolean(attendees);
+    this.review = new Array(review); // Auto-matched volunteers to be reviewed by an admin. 
+    this.include = new Array(include); // Volunteers matched by an admin.
+    this.exclude = new Array(exclude); // Volunteers rejected by an admin.
 }
 
 /*
@@ -29,7 +33,8 @@ Possible skills include Communication, Teamwork, Organized, Adaptaility, Driving
 It may be better to assign each skill an integer ID.
 In the complete project, a volunteer profile can be taken from the data from an existing volunteer account.
 */
-function Volunteer(nameF, nameL, skills, preferences, dates) {
+function Volunteer(id, nameF, nameL, skills, preferences, dates) {
+    this.id = id;
     this.nameF = nameF;
     this.nameL = nameL;
     this.skills = new Array(skills);
@@ -38,67 +43,8 @@ function Volunteer(nameF, nameL, skills, preferences, dates) {
 }
 
 /*
-Function: Fetch volunteer history
-This is used for volunteer_history. It gathers past events to include in displaying volunteering history.
-*/
-function getHistory(volevents){
-    this.volevents = new Array(volevents);
-    if (this.volevents.length() === 0) return 0;
-    else
-        this.volevents.forEach(function(x) {
-            if (x.attendees) then (console.log('ATTENDED ${x.name}'));
-        });
-}
-
-/*
-Function: Fetch matches to existing volunteer events. Return array of available matches, or 0 if no matches were found.
-Look at preexisting future events and see if the volunteer will be a match.
-This is used for volunteer_matching_form.
-For Assignment 3, a hardcoded set of events will be used.
-*/
-function getMatches(volprofile, list, thing){
-    // volprofile = Volunteer Obj., list = every array, thing = extrenal array
-    if (volprofile.length() === 0) return 0;
-    else
-        thing.length = 0; // Clear the array of available events to start anew.
-        // Step 1: Filter events by date. Only include events that the person would likely be available for.
-        for (let i = 0; i < list.length(); i ++){
-            if(list[i].date in volprofile.dates) thing.push(list[i]);
-        }
-        // Step 2: Sort by skill. Prioritize events a volunteer would do best in.
-        if(volprofile.profile.dates.length() >= 0){
-            let x = thing.length();
-            let skill3 = []; // All three skills in the event.
-            let skill2 = []; // 2 of three skills.
-            let skill1 = []; // 1 of three skills.
-            let skill0 = []; // No skills matched.
-            for (let i = 0; i < x; i ++){
-                // Count the number of skills matched.
-                let y = 0;
-                if (thing.skills.length() > 0){
-                    for(let j = 0; j < thing.skills.length(); j ++){
-                        if(things.skills[j] in volprofile.skills) y ++;
-                    }
-                }
-                switch(y){
-                    case 0:
-                        skill0.push(thing[i])
-                    case 1:
-                        skill1.push(thing[i])
-                    case 2:
-                        skill2.push(thing[i])
-                    case 3:
-                        skill3.push(thing[i])
-                }
-            }
-            thing = skill3 + skill2 + skill1 + skill0;
-        }
-        return thing;
-};
-
-/*
 Below are some hard-coded events for Assignment 3.
-Note: Starting with Assignment 4, these will need to be changed from const variables to let variables.
+Note: Starting with Assignment 4, these will need to be taken from a database.
 */
 let event1 = new VolEvent(0, 'Little League Baseball Game', 1, 'Houston, TX', '2025-10-17',
     'At 5:00 PM on October 17th, we will host a Little League baseball game for the Houston area.',
@@ -114,9 +60,112 @@ let event4 = new VolEvent(3, 'Unknown', 0, 'Houston, TX', '2025-10-20',
     [1, 4, 5], true);
 let event5 = new VolEvent(4, 'Little League Baseball Game', 1, 'Houston, TX', '2025-10-24',
     'At 5:00 PM on October 24th, we will host a Little League baseball game for the Houston area.',
-    [1, 4, 5], true);
+    [1, 4, 5], false);
 
 let eventList = [event1, event2, event3, event4, event5];
 
 // Below is a hard-coded volunteer for Assignment 3.
-let Mii = new Volunteer('Jason', 'Adams', [1, 4]); // Jason Adams has skills 1 and 4.
+let Mii1 = new Volunteer(1234, 'Jason', 'Adams', [1, 4], null, ['2025-10-18', '2025-10-19']); // Jason Adams, user ID 1234, has skills 1 and 4.
+// Starting with Assignment 4, this will be replaced with a logged in user.
+
+/*
+Function: Fetch volunteer history
+This is used for volunteer_history. It gathers past events to include in displaying volunteering history.
+The current assumed approach here is to check with the database to see if a user attended.
+*/
+function getHistory(){
+    if (eventList.length() === 0) return 0;
+    let hist = [];
+    for(let i = 0; i < eventList.length(); i ++){
+        if (eventList[i].attendees){
+            (console.log('ATTENDED ${x.name}'))
+            hist.push(eventList[i]);
+        };
+    };
+}
+
+/*
+Function: Fetch matches to existing volunteer events. Return array of available matches, or 0 if no matches were found.
+Look at preexisting future events and see if the volunteer will be a match.
+This is used for volunteer_matching_form on the volunteer's end.
+For Assignment 3, a hardcoded set of events will be used.
+*/
+function getMatches(volprofile, list, thing){
+    // volprofile = Volunteer Obj., list = every array, thing = extrenal array
+    if (volprofile.length() === 0) return 0;
+
+    thing.length = 0; // Clear the array of available events to start anew.
+    // Step 1: Filter events by date. Only include events that the person would likely be available for.
+    for (let i = 0; i < list.length(); i ++){
+        if(~(volprofile.id() in (list[i].exclude)) & volprofile.dates.some((val) => val in list[i].date)) {
+            thing.push(list[i]);
+            list[i].review.push(volprofile.id);
+        }
+    }
+    // Step 2: Sort by skill. Prioritize events a volunteer would do best in.
+    if(volprofile.profile.dates.length() > 0){
+        let x = thing.length();
+        let skill3 = []; // All three skills in the event.
+        let skill2 = []; // 2 of three skills.
+        let skill1 = []; // 1 of three skills.
+        let skill0 = []; // No skills matched.
+        for (let i = 0; i < x; i ++){
+            // Count the number of skills matched.
+            let y = 0;
+            if (thing.skills.length() > 0){
+                for(let j = 0; j < thing.skills.length(); j ++){
+                    if(things.skills[j] in volprofile.skills) y ++;
+                }
+            }
+            switch(y){
+                case 0:
+                    skill0.push(thing[i])
+                case 1:
+                    skill1.push(thing[i])
+                case 2:
+                    skill2.push(thing[i])
+                case 3:
+                    skill3.push(thing[i])
+            }
+        }
+        thing = skill3.concat(skill2, skill1, skill0);
+    }
+    // Step 3: Sort by instant match.
+    let matched = [];
+    for (let i = 0; i < thing.length(); i ++){
+        if(volprofile.id() in (thing[i].include)){
+            matched.push(thing.splice(i, 1));
+            thing = thing.splice(i, 1);
+            i --;
+        }
+    }
+    if (matched.length() > 0){
+        thing = matched.concat(thing);
+    }
+    return thing;
+};
+
+/*
+Function: Provide a list of auto-matched volunteers to be matched or rejected.
+Look at preexisting future events and see if the volunteer will be a match. If so, add them to the event.
+This is used for volunteer_matching_form on the admin's end.
+For Assignment 3, a hardcoded set of events will be used.
+*/
+function setMatches(){
+    // This is where you need to learn how JavaScript can be incorporated into HTML.
+    // Load each event as an element. Each event will have its own sub elements for each voluteer.
+    for (let i = 0; i < eventList.length(); i ++){
+        if(eventList[i].review.length() > 0){
+            // Create Event element
+            for(let j = 0; j < eventList[i].review.length(); j ++){
+                // Create Volunteer Element
+            }
+        }
+    }
+    // The Match and Reject buttons should give a confirmation message.
+    // Upon confirmation, move the volunteer from review to include/exclude. Then, remove the volunteer element.
+        // If there are no volunteer elements left, Replace it with a message.
+        // "No auto-matched voluteers. Looking for others..."
+    // Note: Have it load new volunteers every so often.
+    // Maybe make the events a drop-down menu?
+}
